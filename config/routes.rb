3a443b36c6ed9c6e9f8f0add4351643d2e4ffed3
masao <masao>
2020-11-03 20:58:37 +0900

    3日分のテーブル作成ができました

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :relations
   resources :rooms
   resources :profiles #scaffoldでつくるとこうなる。この記述をすることでユーザー管理機能にアクセスすることができるようになります。
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   root 'comments#index' 
   get 'comments/index' 
 end
+
